e890ac2d4ab17dcd5189c8b7f1367cf055ed630c
Aaron Dangerfield <AaronDangerfield@live.com>
2021-05-07 17:23:10 +0100

    Removed pre-installed .Net Identity & DbContext to reimplement manually (TO LEARN!)

--- PartnerProject/Startup.cs
+++ PartnerProject/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using PartnerProject.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,14 +26,8 @@ namespace PartnerProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
